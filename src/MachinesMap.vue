96fa0b750d71a601a0093921240a4f7fbd63708c
Yo-Ma <yoma@netc.fr>
2018-03-08 00:50:54 +0100

    Etape 7.2 - Ajout de la carte 'google-maps': modif composant MachinesMap.vue & main.js

--- src/MachinesMap.vue
+++ src/MachinesMap.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>  Carte des machines </h1>
-    <gmap-map :center="{lat:10, lng:10}" :zoom="7" style="width: 100%; height: 800px" />
+    <gmap-map :center="{lat:45.188529, lng:5.724523999999974}" :zoom="4" style="width: 100%; height: 800px" />
   </div>
 </template>
 
